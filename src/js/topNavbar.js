7322f661b5eac385aefc808aca0bf4dd9701734c
Vincent Testagrossa <lancevancevt@gmail.com>
2024-01-01 18:03:53 -0500

    playing with navbar within a fluid container

--- src/js/topNavbar.js
+++ src/js/topNavbar.js
@@ -1,16 +1,19 @@
 function loadTopNavbar(){
+    const content = document.createElement('div');
     const navbar = document.createElement('nav');
     const container = document.createElement('div');
     const brand = document.createElement('a');
     brand.setAttribute("href", "#");
     brand.textContent = "Navbar";
-    navbar.setAttribute("class", "navbar navbar-light");
-    container.setAttribute("class", "container-fluid bg-secondary navbar-dark px-3");
+    content.setAttribute("class", "container-fluid m-0 p-0");
+    navbar.setAttribute("class", "navbar navbar-dark bg-dark");
+    container.setAttribute("class", "container-fluid navbar-dark px-5 border-3");
     brand.setAttribute("class", "navbar-brand");
 
+    content.appendChild(navbar);
     navbar.appendChild(container);
     container.appendChild(brand);
 
-    return container;
+    return content;
 }
 export default loadTopNavbar;
