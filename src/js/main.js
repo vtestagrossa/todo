9999f4a8a5cd1154a2645a721a3ab02b820f7172
Vincent Testagrossa <lancevancevt@gmail.com>
2024-01-01 13:21:11 -0500

    playing with javascript and bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
+
+function test() {
+    const container = document.createElement('div');
+    container.classList.add("container", "py-2", "px-5", "secondary");
+    container.textContent = "Testing the container";
+
+    return container;
+}
+
+document.body.appendChild(test());
